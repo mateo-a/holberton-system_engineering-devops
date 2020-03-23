5853f3221f010f15646edd184b2c3e3b16c13752
mateo-a <alexander.urrego@gmail.com>
2020-03-23 16:50:45 -0500

    [ADD] Add Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#Puppet, create a file in /tmp
+file { '/tmp/holberton':
+     path    => '/tmp/holberton',
+     mode    => '0744',
+     owner   => 'www-data',
+     group   => 'www-data',
+     content => 'I love Puppet'
+     }

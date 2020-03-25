f795979e61335b9ad2441d34d44beccb5f138847
mateo-a <alexander.urrego@gmail.com>
2020-03-25 18:11:09 -0500

    [FIX] Fix Task 6

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,7 +10,7 @@ file {'/var/www/html/index.html':
 file_line { 'redirect':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
+  after  => 'server_name _',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
 }
 

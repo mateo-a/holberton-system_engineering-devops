f3bfdc5906a7a278e41e64a455e92ad958920fe6
mateo-a <alexander.urrego@gmail.com>
2020-03-26 14:57:48 -0500

    [FIX] Fix Task 6

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,5 @@
 # Install Nginx web server (w/ Puppet)
-package { 'nginx':
-ensure => installed,
-}
-
-file { '/var/www/html/index.html':
-content => 'Holberton School',
-}
-
-file_line { 'redirect':
-path   => '/etc/nginx/sites-available/default',
-ensure => 'present',
-after  => 'listen 80 default_server',
-line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-service { 'nginx':
-ensure  => running,
-require => Package['nginx'],
+exec { 'installNginx':
+  command  =>  'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo \"Holberton School\" | sudo tee /var/www/html/index.nginx-debian.html ;  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  provider =>  shell,
 }
